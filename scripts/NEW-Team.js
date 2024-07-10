379bffc1ba50a33314a6cdc34f00cb6ec2a020f9
Anna Sund <suchi@annas-mbp-3.lan>
2024-07-11 00:48:03 +0200

    improvements for getting & rendering data/dom elements in teams

--- scripts/NEW-Team.js
+++ scripts/NEW-Team.js
@@ -1,3 +1,4 @@
+// NB: remember to add new year(s) at the START OF ARRAY! (at: function createYearOptions())
 const dd_selected = {
     selectedYear: '',
     selectedCategory: ''
@@ -7,6 +8,7 @@ window.onload = function(){
     createYearOptions();
     createCategoryOptions();
 };
+
 
 function setDdColor(liElement, relevantArray){
     for (let i = 0; i < relevantArray.length; i++) {
@@ -32,9 +34,7 @@ function setYearContent(selectedYear, yearsArray){
     const t = document.getElementById(`ddOpt_${selectedYear}`);
     setDdColor(t, yearsArray);
 
-    console.log(`STORED: ${dd_selected.selectedYear}`);
-    console.log(`SELECTED: ${selectedYear}`);
-
+    
     // DOM STUFFS FOR THE CARDS
     if (dd_selected.selectedYear == '') {
         getMembersData(selectedYear, 0);
@@ -51,7 +51,7 @@ function setYearContent(selectedYear, yearsArray){
 };
 function createYearOptions(){
     // NB: remember to add new year at the START OF ARRAY!
-    let years = [2024, 2023];
+    let years = [2024, 2023, 2016];
 
     const yearSelectDiv = document.getElementById('yearSelect_Dd');
     const year_ul = document.createElement('ul');
@@ -66,7 +66,6 @@ function createYearOptions(){
         year_li.id = `ddOpt_${years[i]}`;
         year_li.onclick = () => setYearContent(years[i], years);
     };
-    
     setYearContent(years[0], years); // Default
 };
 
@@ -98,7 +97,6 @@ function createCategoryOptions(){
 
 
 function clear_Html_Content(child){
-    console.log('DELETE');
     child.remove();  
 };
 function create_Html_div(divClass) {
@@ -108,73 +106,103 @@ function create_Html_div(divClass) {
 };
 
 
-function create_Html_memberCard(deptID, memberName, memberRoles, imgCode, deptName, year){
-    let deptId_txt = '';
-    switch (deptID) {
-        case 1:
-            deptId_txt = 'boardMembers';
-            break;
-        case 2:
-            deptId_txt = 'adminMembers';
-            break;
-        case 3:
-            deptId_txt = 'mechMembers';
-            break;
-        case 4:
-            deptId_txt = 'eleMembers';
-            break;
-        case 5:
-            deptId_txt = 'dataMembers';
-            break;
-        default:
-            break;
-    };
-    let membersContainer = document.getElementById(deptId_txt);
-
-    let card = create_Html_div('membercard');
-    let cardContentDiv = create_Html_div('membercard_content');
-    // IMG
-    let cardImg = document.createElement('img');
-    cardImg.src = `/img/teams/members/${year}/${deptName}/${imgCode}.jpg`;
-
-    let infoDiv = create_Html_div('member_info');
-    let memberName_p = document.createElement('p');
-    memberName_p.className = 'name_p';
-    memberName_p.textContent = memberName;
-    let memberRole_p = document.createElement('p');
-    memberRole_p.textContent = memberRoles;
-
-    membersContainer.appendChild(card);
-    card.appendChild(cardContentDiv);
-    cardContentDiv.appendChild(cardImg);
-    cardContentDiv.appendChild(infoDiv);
-    infoDiv.appendChild(memberName_p);
-    infoDiv.appendChild(memberRole_p);
-};
-
-
-// Getting JSON-data
+// Getting JSON-data and displaying it
 function getMembersData(year, yearStatus){
-    // DO SOME CHECKING - IF THERE IS CONTENT FROM BEFORE: delete it before updating with rendering new content
+    let team_showcaseDiv = document.getElementById('team_showcase');
     switch (yearStatus) {
-        case 0: // FALSE
+        case 0: // FALSE --> DOM content doesn't exist, Create & Render DOM content
+            let df = new DocumentFragment(); // what to render to the DOM
+
+            // FOR LOOP for opt array
             fetch(`/websiteData/teams/${year}-team.json`).then(res => res.json()).then(data => {
-                data.forEach(element => {
-                    let deptID = element.deptID;
-                    let deptName = element.deptName;
-                    let members = element.members;
-                
+                data.forEach(dept => { 
+                    let deptID = dept.deptID;
+                    let deptName = dept.deptName;
+                    let members = dept.members;
+
+                    let deptTitle_txt = '';
+                    switch (deptID) {
+                        case 1:
+                            deptTitle_txt = 'The Board';
+                            break;
+                        case 2:
+                            deptTitle_txt = 'Administration';
+                            break;
+                        case 3:
+                            deptTitle_txt = 'Mechanical';
+                            break;
+                        case 4:
+                            deptTitle_txt = 'Electronics';
+                            break;
+                        case 5:
+                            deptTitle_txt = 'Data';
+                            break;
+                    };
+
+                    //Create section-part w/ divider and div for membercards
+                    let teamMembers_section = document.createElement('section'); 
+                    teamMembers_section.id = `${deptName}_section`;
+                    teamMembers_section.className = 'teamMembersSection';
+
+                    // Divider
+                    let divider_div = document.createElement('div');
+                    divider_div.classList.add('section_divider', 'flex_center');
+                    let dividerStart_div = create_Html_div('divider');
+                    let dividerStart_hr = document.createElement('hr');
+                    let deptName_h4 = document.createElement('h4');
+                    deptName_h4.className = 'team_showcase';
+                    deptName_h4.textContent = deptTitle_txt;
+                    let dividerEnd_div = create_Html_div('divider');
+                    let dividerEnd_hr = document.createElement('hr');
+
+                    // Section-content
+                    let sectionContent_div = create_Html_div('section_content');
+                    let membersContainer_div = create_Html_div('members_container');
+                    membersContainer_div.id = `${deptName}Members`;
+
+                    // Merging the DOM elements
+                    teamMembers_section.appendChild(divider_div);
+                    divider_div.appendChild(dividerStart_div);
+                    dividerStart_div.appendChild(dividerStart_hr);
+                    divider_div.appendChild(deptName_h4);
+                    divider_div.appendChild(dividerEnd_div);
+                    dividerEnd_div.appendChild(dividerEnd_hr);
+                    teamMembers_section.appendChild(sectionContent_div);
+                    sectionContent_div.appendChild(membersContainer_div);
+
+                    df.appendChild(teamMembers_section);
+
                     members.forEach(member => {
-                            create_Html_memberCard(deptID, member.name, member.roles, member.imgCode, deptName, year);
+                        let card = create_Html_div('membercard');
+                        let cardContentDiv = create_Html_div('membercard_content');
+                    
+                        // IMG
+                        let cardImg = document.createElement('img');
+                        cardImg.src = `/img/teams/members/${year}/${deptName}/${member.imgCode}.jpg`;
+                    
+                        let infoDiv = create_Html_div('member_info');
+                        let memberName_p = document.createElement('p');
+                        memberName_p.className = 'name_p';
+                        memberName_p.textContent = member.name;
+                        let memberRole_p = document.createElement('p');
+                        memberRole_p.textContent = member.roles;
+                    
+                        membersContainer_div.appendChild(card);
+                        card.appendChild(cardContentDiv);
+                        cardContentDiv.appendChild(cardImg);
+                        cardContentDiv.appendChild(infoDiv);
+                        infoDiv.appendChild(memberName_p);
+                        infoDiv.appendChild(memberRole_p);
                     });
                 });
+                team_showcaseDiv.appendChild(df);
             });
-            break;
-        case 1: // TRUE
-            let elements = document.querySelectorAll('.membercard');
-            elements.forEach(function(element) {
-              clear_Html_Content(element);
-            });
+            break; 
+        case 1: // TRUE --> DOM content exists, delete it
+            let membersSections = document.querySelectorAll('.teamMembersSection');
+            membersSections.forEach(function(section) {
+                clear_Html_Content(section);
+              });
             break;
     };
 };
