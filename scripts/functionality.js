444056dac4d2e8f29310fea4e8465e5b0bff4cfd
AnSuChi <anutika02@gmail.com>
2024-10-01 18:41:43 +0200

    make navbar visible all the time, remove hide/unhide effect

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -31,27 +31,6 @@ navToggle.addEventListener('click', () => {
 
 });
 
-
-// Controls the visibility of the header, depending on position and scrolling
-let last_scroll_position = 0;
-window.addEventListener('scroll', () => {
-    const current_scroll_position = window.scrollY;
-
-    if (current_scroll_position === 0 || current_scroll_position <= 25){
-        body.classList.add('scroll_up');
-    }
-    else if (current_scroll_position > last_scroll_position && !body.classList.contains('scroll_down')){
-        body.classList.remove('scroll_up');
-        body.classList.add('scroll_down');
-    }
-    else if (current_scroll_position < last_scroll_position && body.classList.contains('scroll_down')){
-        body.classList.remove('scroll_down');
-        body.classList.add('scroll_up');
-    }
-    last_scroll_position = current_scroll_position;
-})
-
-
 // Auto update copyright year
 let currentYear = new Date().getFullYear();
 document.getElementById('copyYear').textContent = currentYear;
